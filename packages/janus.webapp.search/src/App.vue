e248cf7c1a9b4deb0b270e61e9dc91321787822a
Vinicius Politta <vinicius@janusproj.com>
2019-04-23 18:37:18 -0300

    Layout Search bar
    JP-346 - ajuste layout

--- packages/janus.webapp.search/src/App.vue
+++ packages/janus.webapp.search/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/eth1.jpg">
     <router-view/>
   </div>
 </template>
--- packages/janus.webapp.search/src/components/HelloWorld.vue
+++ packages/janus.webapp.search/src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="hello">
+  <div class="form-group">
     <h1>{{ msg }}</h1>
     <div class="form-group">
       <input type="text" class="form-control" id="searchbar" placeholder="Search Here">
+      <button type="submit" class="btn">&#128269;</button>
+      
     </div>
   </div>
 </template>
@@ -12,7 +14,7 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Janus Project"
     };
   }
 };
@@ -23,11 +25,22 @@ export default {
 .form-control {
   width: 400px;
   border-radius: 20px;
-  left: 55%;
+  left: 43%;
   margin: -130px 0 0 -210px;
-  width: 700px;
-  
+  width: 700px;  
   position: absolute;
   top: 50%;
+  
 }
+.btn{
+  width: 80px;
+  border-radius: 20px;
+  left: 77%;
+  margin: -130px 0 0 -210px;
+  width: 49px;  
+  position: absolute;
+  top: 50%;
+  border-color: transparent;
+}
+
 </style>
