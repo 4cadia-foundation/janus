b7c2dbf9069e9ea957938cafeabae3c54cf39a56
Bianca Kaiser <bianca@janusproj.com>
2019-04-24 15:56:08 -0300

    Adicionando web3 ao app

--- packages/janus.webapp.search/src/components/HelloWorld.vue
+++ packages/janus.webapp.search/src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
 
 <script>
 import InputSearch from '@/components/input'
+import { default as Web3 } from 'web3'
 
 export default {
   name: 'HelloWorld',
@@ -19,7 +20,19 @@ export default {
   },
   components: {
     'input-search': InputSearch
-
+  },
+  mounted () {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    let web3 = window.web3
+    if (typeof web3 !== 'undefined') {
+      console.warn('Using web3 detected from external source.')
+      // Use Mist/MetaMask's provider
+      // web3 = new Web3(web3.currentProvider)
+      web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+      web3.eth.getAccounts(console.log)
+    } else {
+      console.warn('No web3 detected.')
+    }
   }
 }
 </script>
