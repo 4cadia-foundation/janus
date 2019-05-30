e80295ed5f211d2fecc52b119e733ed75010c2a9
Victoria Duraes <victoria@janusproj.com>
2019-05-30 21:39:53 +0000

    Merged in feature/JPI-41 (pull request #1)
    Feature/JPI-41
    
    Approved-by: João Bombardelli <jc@janusproj.com>

--- packages/janus.webapp.indexer/config/prod.env.js
+++ packages/janus.webapp.indexer/config/prod.env.js
@@ -1,5 +1,9 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  IDENTITY_BASE_URL: '"http://localhost:5000/api/auth"'
+  IDENTITY_BASE_URL: '"http://localhost:5000/api/auth"',
+  IDENTITY: true,
+  CIVICID: '"-uXno0-XF"',
+  DISABLE_IDENTITY_CIVIC: false,
+  DISABLE_IDENTITY_METAMASK: true
 }
--- packages/janus.webapp.indexer/config/test.env.js
+++ packages/janus.webapp.indexer/config/test.env.js
@@ -4,5 +4,9 @@ const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  IDENTITY_BASE_URL: '"http://localhost:5000/api/auth"'
+  IDENTITY_BASE_URL: '"http://localhost:5000/api/auth"',
+  IDENTITY: false,
+  CIVICID: '"-uXno0-XF"',
+  DISABLE_IDENTITY_CIVIC: false,
+  DISABLE_IDENTITY_METAMASK: true
 })
